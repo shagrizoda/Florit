953391f8e55f3e010240d9bcf190b93fd3e5315e
shagrizoda <shahrizodae@gmail.com>
2022-07-26 14:52:32 +0500

    initial commit

--- Florit/ViewController.swift
+++ Florit/ViewController.swift
@@ -6,14 +6,119 @@
 //
 
 import UIKit
+import CoreML
+import Vision
+import Alamofire
+import SwiftyJSON
+import SDWebImage
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var flowerInfo: UILabel!
+    
+    let wikipediaURl = "https://en.wikipedia.org/w/api.php"
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = true
+        
+        
+        
     }
+    
+    
 
-
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let userPickedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage{
+            
+            
+            guard let ciimage = CIImage(image: userPickedImage) else {
+                fatalError("Could not convert to CIImage")
+            }
+            detect(image: ciimage)
+        }
+        
+        imagePicker.dismiss(animated: true)
+        
+    }
+    
+    func detect(image: CIImage){
+        guard let model = try? VNCoreMLModel(for: FlowerClassifier().model) else{
+            fatalError("Loading CoreML model failed")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { request, error in
+            guard let results = request.results as? [VNClassificationObservation] else{
+                fatalError("Model failed to process image")
+            }
+            
+            if let firstResult = results.first{
+                self.navigationItem.title = firstResult.identifier.capitalized
+                self.requestInfo(flowerName: firstResult.identifier)
+                
+            }
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
+    }
+    
+    func requestInfo(flowerName: String){
+        let parameters : [String:String] = [
+        "format" : "json",
+        "action" : "query",
+        "prop" : "extracts|pageimages",
+        "exintro" : "",
+        "explaintext" : "",
+        "titles" : flowerName,
+        "indexpageids" : "",
+        "redirects" : "1",
+        "pithumbsize" : "500"
+        ]
+        
+        Alamofire.request(wikipediaURl, method: .get, parameters: parameters).responseJSON {
+            (response) in
+            if response.result.isSuccess{
+                print("Successfully got info from wikipedia")
+                print(response)
+                
+                let flowerJSON: JSON = JSON(response.result.value!)
+                
+                let pageid = flowerJSON["query"]["pageids"][0].stringValue
+                
+                let flowerDescription = flowerJSON["query"]["pages"][pageid]["extract"].stringValue
+                
+                let flowerImageURL = flowerJSON["query"]["pages"][pageid]["thumbnail"]["source"].stringValue
+                
+                self.imageView.sd_setImage(with: URL(string: flowerImageURL))
+                
+                self.flowerInfo.text = flowerDescription
+                
+               
+                
+                
+                
+            }
+        }
+        
+    }
+    
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
 }
 
---
+++ README.md
@@ -0,0 +1,18 @@
+# Florit
+**Flower recognition app**
+
+build by using the convertion of **oxford102.caffemodel** to **coreML**
+
+pods used:
+- Alamofire
+- SwiftyJSON
+- SDWebImage
+
+App operates in the following way:
+1. Camera opens after clicking the camera button (UIImagePickerController() used)
+2. Image picker allows editing
+3. Name of the flower along with the brief information and picture from wikipedia is shown (using JSON)
+
+<img width="500" height="600" alt="florit img" src="https://user-images.githubusercontent.com/71122864/180757900-7c679f8e-cc4e-4248-815f-40929739650e.png"> <img width="500" height="600" alt="florit_2" src="https://user-images.githubusercontent.com/71122864/180758898-1f32837f-c2ee-46c8-a23f-36cc72c9d07b.png">
+
+
